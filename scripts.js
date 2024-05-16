fbad82780877a6dc8075133f219c85e6718e666b
JacksonRMS <jacktjrubem@gmail.com>
2024-05-16 17:33:41 -0300

    Desenvolvimento da aplicação

--- scripts.js
+++ scripts.js
@@ -1,14 +1,38 @@
 const botaoConversor = document.querySelector("#botao")
-
 botaoConversor.addEventListener("click", conversorDeValores)
 
-function conversorDeValores(){
+const seletor = document.querySelector("#seletorDeMoeda")
+
+
+function conversorDeValores() {
     const inputConversor = document.querySelector("#valorAConverter").value
 
+    const valorEmReal = document.querySelector("#valorParaConverter")
+    const moedas = document.querySelector("#valorConvertido")
+
     const dolarDoDia = 6
+    const euroDoDia = 7
 
-    const resultadoConvertido = inputConversor / dolarDoDia 
 
-    console.log(resultadoConvertido)
+    if (seletor.value == 'dolar') {
+        //Se o select estiver selecionando o valor de DOLAR, essa condição entra!
+        moedas.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputConversor / dolarDoDia)
+    }
+
+    if (seletor.value == 'euro') {
+        //Se o select estiver selecionando o valor de EURO, essa condição entra!
+        moedas.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputConversor / euroDoDia)
+    }
+
+    valorEmReal.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputConversor)
 }
 
