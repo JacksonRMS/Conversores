8669e228e80dce841fedeea2522df08112d4e6f9
JacksonRMS <jacktjrubem@gmail.com>
2024-05-20 17:29:54 -0300

    Projeto do curso finalizado. Inicio dos outros conversores

--- scripts.js
+++ scripts.js
@@ -1,14 +1,36 @@
-const botaoConversor = document.querySelector("#botao")
-botaoConversor.addEventListener("click", conversorDeValores)
+const botaoConversor = document.querySelector("#botao")//Botão selecionado
+botaoConversor.addEventListener("click", conversorDeValores)//Evento adicionado ao botão
 
-const seletor = document.querySelector("#seletorDeMoeda")
+const seletor = document.querySelector("#seletorDeMoeda")//elemento 'select' selecionado
+seletor.addEventListener("change", trocaDeSeletor)// Toda vez que o seletor for trocado ele chamará 
+//função abaixo "trocaDeSeletor"
+function trocaDeSeletor() {
+    const nomeDaMoeda = document.getElementById("dolares")
+    const imagem = document.getElementById("bandeira")
+    const fundo = document.getElementById("fundo")
 
+    if (seletor.value == 'dolar'){
+        nomeDaMoeda.innerHTML = "mexirica"
+    imagem.src = "./assets/dolar.png"
+    
+    }
+    
+    if (seletor.value == 'euro'){
+        nomeDaMoeda.innerHTML = "batata"
+    imagem.src = "./assets/euro.png"
+    fundo.style.backgroundColor = "#857744"
+    fundo.style.backgroundColor = "#852394"
+}
+
+conversorDeValores()
+
+}
 
 function conversorDeValores() {
-    const inputConversor = document.querySelector("#valorAConverter").value
+    const inputConversor = document.querySelector("#valorAConverter").value//Selecionando apenas o valor do input identificado
 
-    const valorEmReal = document.querySelector("#valorParaConverter")
-    const moedas = document.querySelector("#valorConvertido")
+    const valorEmReal = document.querySelector("#valorParaConverter") //Paragrafo com o valor a ser convertido
+    const moedas = document.querySelector("#valorConvertido")//Paragrafo com o valor convertido
 
     const dolarDoDia = 6
     const euroDoDia = 7
@@ -24,13 +46,13 @@ function conversorDeValores() {
 
     if (seletor.value == 'euro') {
         //Se o select estiver selecionando o valor de EURO, essa condição entra!
-        moedas.innerHTML = new Intl.NumberFormat("en-US", {
+        moedas.innerHTML = new Intl.NumberFormat("de-DE", { // O comando "Intl" formata o valor para a moeda
             style: "currency",
-            currency: "USD"
+            currency: "EUR"
         }).format(inputConversor / euroDoDia)
     }
 
-    valorEmReal.innerHTML = new Intl.NumberFormat("pt-BR", {
+    valorEmReal.innerHTML = new Intl.NumberFormat("pt-BR", {// O comando "Intl" formata o valor para a moeda selecionada
         style: "currency",
         currency: "BRL"
     }).format(inputConversor)
