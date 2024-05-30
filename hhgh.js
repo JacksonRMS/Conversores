0ac851ea6946d7cbedda1d90c94d684641d2ea27
JacksonRMS <jacktjrubem@gmail.com>
2024-05-29 21:07:17 -0300

    Procurando o erro da conversão de 'c' para 'k'

--- hhgh.js
+++
@@ -1,7 +0,0 @@
-const celcius = 40
-const fah = 9/5+32
-// const fa1 = 32
-
-const resultado = celcius*fah+32
-
-console.log(resultado)
--- temperaturas.js
+++ temperaturas.js
@@ -1,36 +1,40 @@
 const botaoConversor = document.querySelector("#botao")//Botão selecionado
- botaoConversor.addEventListener("click", conversorDeValores)//Quando o botão é clicado ele chama a função-
- //"conversorDeValores" e apresenta a menssagem
+botaoConversor.addEventListener("click", conversorDeValores)//Quando o botão é clicado ele chama a função-
+//"conversorDeValores" e apresenta a menssagem
 
- 
 
- const seletorDeTemperatura = document.querySelector("#seletorDeTemperatura")//elemento 'select' selecionado
+
+const seletorDeTemperatura = document.querySelector("#seletorDeTemperatura")//elemento 'select' selecionado
 // seletorDeTemperatura.addEventListener("change", trocaDeSeletor)// Toda vez que o seletor for trocado ele chamará 
 
- function conversorDeValores() {
-   const inputConversor = document.querySelector("#valorAConverter").value //Selecionando apenas o valor do input identificado
-   const fahrenheit = 9 / 5
-   const resultado = inputConversor * fahrenheit + 32
+function conversorDeValores() {
+  const inputConversor = document.querySelector("#valorAConverter").value //Selecionando apenas o valor do input identificado
 
-   const tempo = document.querySelector("#valorConvertido")
+  const fahrenheit = 9 / 5
+  const kel = 273.15
+
+
+  const tempo = document.querySelector("#valorConvertido")
 
    if (seletorDeTemperatura.value == 'dolar') {
-       tempo.innerHTML = resultado
-          }
+     tempo.innerHTML = new Intl.NumberFormat("pt-BR", { // O comando "Intl" formata o valor para a moeda
+       style: "unit",
+       unit: "fahrenheit"
+   }).format(inputConversor*fahrenheit+32)
+   }
 
-   console.log(resultado)
- }
- 
-    const tempo = document.querySelector("#valorConvertido")
+   if (seletorDeTemperatura.value == 'euro') {
+     tempo.innerHTML = new Intl.NumberFormat("pt-BR", { // O comando "Intl" formata o valor para a moeda
+       style: "unit",
+        unit: "kelvin"
+   }).format(inputConversor + kel)// Erro ainda indefinido...
+   }
 
-//    const fahrenheit = 9 / 5
-//    const resultado = inputConversor * fahrenheit + 32
 
-//    if (seletorDeTemperatura.value == 'dolar') {
-//       tempo.innerHTML.text = resultado
-//    }
+  
+}
 
-// }
+
 
 
 
